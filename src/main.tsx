23968f727285b66edc046da12d2233d0bd497493
Dmitry Bogdanov <dmtribogdanov@gmail.com>
2026-06-14 15:08:33 +0500

    query запрос к плейлистам и отрисовкаÐÐ

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './app/ui/App/App.tsx'
 import {BrowserRouter} from "react-router";
+import {Provider} from "react-redux";
+import {store} from "@/app/model/store";
+import App from "@/app/ui/App/App";
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 )
