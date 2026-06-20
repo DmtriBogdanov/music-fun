4fb1477a9c97619613fb817d229b5089fc9ca0bc
Dmitry Bogdanov <dmtribogdanov@gmail.com>
2026-06-20 07:01:52 +0500

    Поиск

--- src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
@@ -23,9 +23,11 @@ import {
 export const PlaylistsPage = () => {
   // 1
   const [playlistId, setPlaylistId] = useState<string | null>(null)
+  const [search, setSearch] = useState('')
+
   const {register, handleSubmit, reset} = useForm<UpdatePlaylistArgs>()
 
-  const {data} = useFetchPlaylistsQuery()
+  const {data, isLoading} = useFetchPlaylistsQuery({search})
   const [deletePlaylist] = useDeletePlaylistMutation()
 
   const deletePlaylistHandler = (playlistId: string) => {
@@ -53,7 +55,13 @@ export const PlaylistsPage = () => {
     <div className={s.container}>
       <h1>Playlists page</h1>
       <CreatePlaylistForm />
+      <input
+        type="search"
+        placeholder={'Search playlist by title'}
+        onChange={e => setSearch(e.currentTarget.value)}
+      />
       <div className={s.items}>
+        {!data?.data.length && !isLoading && <h2>Playlist not found</h2>}
         {data?.data.map(playlist => {
           // 2
           const isEditing = playlistId === playlist.id
