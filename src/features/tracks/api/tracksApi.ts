ec4c709da3434990cadda85175c98f9149428a80
Dmitry Bogdanov <dmtribogdanov@gmail.com>
2026-06-27 06:56:13 +0500

    Infinity Queries

---
+++ src/features/tracks/api/tracksApi.ts
@@ -0,0 +1,24 @@
+import {baseApi} from "@/app/api/baseApi";
+import type {FetchTracksResponse} from "@/features/tracks/api/tracksApi.types";
+
+export const tracksApi = baseApi.injectEndpoints({
+  endpoints: build => ({
+    fetchTracks: build.infiniteQuery<FetchTracksResponse, void, string | undefined>({
+      infiniteQueryOptions: {
+        initialPageParam: undefined,
+        getNextPageParam: (lastPage) => {
+          return lastPage.meta.nextCursor || undefined
+        }
+      },
+      query: ({pageParam}) => ({
+        url: 'playlists/tracks',
+        params: {
+          cursor: pageParam,
+          paginationType: "cursor",
+          pageSize: 5
+        }
+      }),
+    }),
+  }),
+})
+export const { useFetchTracksInfiniteQuery } = tracksApi
